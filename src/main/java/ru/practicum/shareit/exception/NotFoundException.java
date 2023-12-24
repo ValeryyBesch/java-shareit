b2aad8f020f41fd232c0502b0a1730c740821f62
Valera <valerapro.66@yandex.ru>
2023-12-24 20:57:05 +0300

    create: 13 sprint

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -5,6 +5,7 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(Class<?> entityClass, String message) {
         super("Entity " + entityClass.getSimpleName() + " not found. " + message);
     }
+
     public NotFoundException(String message) {
         super(message);
     }
