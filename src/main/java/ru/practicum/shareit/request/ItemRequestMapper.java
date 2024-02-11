cda9cabfef42c60d6e4f7a46d4fca4e55dd19741
Valera <valerapro.66@yandex.ru>
2024-02-11 21:34:40 +0300

    create: 15 sprint

--- src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
@@ -4,9 +4,7 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class ItemRequestMapper {
 
